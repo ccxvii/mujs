f725c8cf9285dd3775292d2c177c8191b07afb46
Tor Andersson <tor@ccxvii.net>
2014-02-05 06:00:51 +0100

    Fix bug in js_rot.

--- jsrun.c
+++ jsrun.c
@@ -308,7 +308,7 @@ void js_rot(js_State *J, int n)
 {
 	int i;
 	js_Value tmp = STACK[TOP-1];
-	for (i = 1; i <= n; ++i)
+	for (i = 1; i < n; ++i)
 		STACK[TOP-i] = STACK[TOP-i-1];
 	STACK[TOP-i] = tmp;
 }
