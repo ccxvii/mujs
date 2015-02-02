c1ad1ba1e482e7d01743e3f4f9517572bebf99ac
Tor Andersson <tor.andersson@artifex.com>
2015-02-02 17:27:07 +0100

    Fix mistake when compiling array literals.
    The values were being used as keys, and vice versa.

--- jscompile.c
+++ jscompile.c
@@ -265,8 +265,8 @@ static void carray(JF, js_Ast *list)
 	int i = 0;
 	while (list) {
 		if (list->a->type != EXP_UNDEF) {
-			cexp(J, F, list->a);
 			emitnumber(J, F, i++);
+			cexp(J, F, list->a);
 			emit(J, F, OP_INITPROP);
 		} else {
 			++i;
