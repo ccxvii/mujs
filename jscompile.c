ce02a9d72953bf65c357d0fcd62cb2d1cfb49808
Tor Andersson <tor.andersson@artifex.com>
2019-03-08 10:56:52 +0100

    Handle empty scripts.

--- jscompile.c
+++ jscompile.c
@@ -1485,5 +1485,5 @@ js_Function *jsC_compilefunction(js_State *J, js_Ast *prog)
 
 js_Function *jsC_compilescript(js_State *J, js_Ast *prog, int default_strict)
 {
-	return newfun(J, prog->line, NULL, NULL, prog, 1, default_strict);
+	return newfun(J, prog ? prog->line : 0, NULL, NULL, prog, 1, default_strict);
 }
